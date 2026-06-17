61e0af43bca2a15c6c7dfa285c035c204782bb7c
sarobidy <hei.sarobidy.6pgmail.com>
2026-06-17 11:31:25 +0300

    updating file

--- src/main/java/library/app/com/entity/Book.java
+++ src/main/java/library/app/com/entity/Book.java
@@ -1,17 +1,38 @@
 package library.app.com.entity;
 
+import jakarta.persistence.*;
 import library.app.com.entity.enums.Genre;
 import lombok.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+@Entity
+@Table(name = "book")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(unique = true, nullable = false)
     private String isbn;
+
+    @Column(nullable = false)
     private BigDecimal price;
+
     private String language;
+
+    @Enumerated(EnumType.STRING)
     private Genre genre;
+
     private LocalDate publishedDate;
 }
