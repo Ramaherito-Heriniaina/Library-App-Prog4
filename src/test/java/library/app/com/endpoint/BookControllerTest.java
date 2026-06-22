3fed2eb3092db5e19574f50d27b36ce74fcc1780
Heriniaina <ramaheritoheriniaina@.com>
2026-06-22 11:42:16 +0300

    Feat: add the test controller file

---
+++ src/test/java/library/app/com/endpoint/BookControllerTest.java
@@ -0,0 +1,26 @@
+package library.app.com.endpoint;
+
+import library.app.com.endpoint.rest.controller.health.BookController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(BookController.class)
+public class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void should_get_all_books() throws Exception {
+        mockMvc.perform(get("/books")
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk());
+    }
+}
